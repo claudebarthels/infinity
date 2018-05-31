22ad2468375213877d1f2165d799609e112c0b8b
Claude Barthels <claudeb@inf.ethz.ch>
2018-05-31 11:18:42 +0200

    Added library header file

---
+++ src/infinity/infinity.h
@@ -0,0 +1,26 @@
+/**
+ * Infinity - A C++ RDMA library for InfiniBand
+ *
+ * (c) 2018 Claude Barthels, ETH Zurich
+ * Contact: claudeb@inf.ethz.ch
+ *
+ */
+
+#ifndef INFINITY_H_
+#define INFINITY_H_
+
+#include <infinity/core/Context.h>
+#include <infinity/core/Configuration.h>
+#include <infinity/memory/Atomic.h>
+#include <infinity/memory/Buffer.h>
+#include <infinity/memory/Region.h>
+#include <infinity/memory/RegionToken.h>
+#include <infinity/memory/RegionType.h>
+#include <infinity/memory/RegisteredMemory.h>
+#include <infinity/queues/QueuePair.h>
+#include <infinity/queues/QueuePairFactory.h>
+#include <infinity/requests/RequestToken.h>
+#include <infinity/utils/Address.h>
+#include <infinity/utils/Debug.h>
+
+#endif /* INFINITY_H_ */
